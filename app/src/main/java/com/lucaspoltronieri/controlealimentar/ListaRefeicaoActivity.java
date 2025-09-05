9bf12c6eb1fe516c6f5ee084fed7c2cade124b7b
Lucas Poltronieri <lucaspoltronierisampaio@gmail.com>
2025-09-05 16:11:57 -0300

    Insert menu context

--- app/src/main/java/com/lucaspoltronieri/controlealimentar/ListaRefeicaoActivity.java
+++ app/src/main/java/com/lucaspoltronieri/controlealimentar/ListaRefeicaoActivity.java
@@ -1,17 +1,22 @@
 package com.lucaspoltronieri.controlealimentar;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,8 +25,106 @@ public class ListaRefeicaoActivity extends AppCompatActivity {
 
     private ListView listViewRefeicao;
     private List<Refeicao> listaRefeicao;
-
     private RefeicaoAdapter refeicaoAdapter;
+    private int posicaoSelecionada = -1;
+
+    private ActionMode actionMode;
+    private View viewSelecionada;
+    private ActionMode.Callback callback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.refeicao_item_menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+
+            int idMenuItem = item.getItemId();
+
+            if (idMenuItem == R.id.menuItemEditar) {
+                mode.finish();
+                editarRefeicao();
+                return true;
+
+            } else if (idMenuItem == R.id.menuItemExcluir) {
+                excluirRefeicao();
+                mode.finish();
+                return true;
+
+            } else {
+                return false;
+            }
+
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            if(viewSelecionada !=null){
+                viewSelecionada.setBackgroundColor(Color.TRANSPARENT);
+
+            }
+            actionMode = null;
+            viewSelecionada = null;
+            listViewRefeicao.setEnabled(true);
+
+        }
+    };
+
+    ActivityResultLauncher<Intent> launcherNovaRefeicao =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                            if (result.getResultCode() == ListaRefeicaoActivity.RESULT_OK) {
+                                Intent intent = result.getData();
+                                Bundle bundle = intent.getExtras();
+                                if (bundle != null) {
+                                    int tipoRefeicao = bundle.getInt(RefeicaoActivity.KEY_TIPOREFEICAO);
+                                    String data = bundle.getString(RefeicaoActivity.KEY_DATA);
+                                    int apetite = bundle.getInt(RefeicaoActivity.KEY_APETITE);
+                                    boolean foraHorario = bundle.getBoolean(RefeicaoActivity.KEY_FORAHORARIO);
+
+                                    Refeicao refeicao = new Refeicao(tipoRefeicao, data, apetite, foraHorario);
+                                    listaRefeicao.add(refeicao);
+                                    refeicaoAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                    });
+
+    ActivityResultLauncher<Intent> launcherEditarRefeicao =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                            if (result.getResultCode() == ListaRefeicaoActivity.RESULT_OK) {
+                                Intent intent = result.getData();
+                                Bundle bundle = intent.getExtras();
+                                if (bundle != null) {
+                                    int tipoRefeicao = bundle.getInt(RefeicaoActivity.KEY_TIPOREFEICAO);
+                                    String data = bundle.getString(RefeicaoActivity.KEY_DATA);
+                                    int apetite = bundle.getInt(RefeicaoActivity.KEY_APETITE);
+                                    boolean foraHorario = bundle.getBoolean(RefeicaoActivity.KEY_FORAHORARIO);
+
+                                    Refeicao refeicao = listaRefeicao.get(posicaoSelecionada);
+                                    refeicao.setTipoRefeicao(tipoRefeicao);
+                                    refeicao.setData(data);
+                                    refeicao.setApetite(apetite);
+                                    refeicao.setForaHorario(foraHorario);
+
+                                    refeicaoAdapter.notifyDataSetChanged();
+                                }
+                            }
+                            posicaoSelecionada = -1;
+                        }
+                    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,73 +134,87 @@ public class ListaRefeicaoActivity extends AppCompatActivity {
 
         listViewRefeicao = findViewById(R.id.listViewRefeicao);
 
+        popularListaRefeicao();
+        //registerForContextMenu(listViewRefeicao);
 
-        listViewRefeicao.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent,
-                                    View view,
-                                    int position,
-                                    long id) {
-                Refeicao refeicao = (Refeicao) listViewRefeicao.getItemAtPosition(position);
-
-                String[] tipoRefeicao = getResources().getStringArray(R.array.tiposRefeicao);
-
-                String selected = getString(R.string.tipo_refeicao) + ": " + tipoRefeicao[refeicao.getTipoRefeicao()] + "\n"
-                        + getString(R.string.clicada);
-
-                Toast.makeText(getApplicationContext(), selected, Toast.LENGTH_LONG).show();
-            }
+        // Clique curto = editar
+        listViewRefeicao.setOnItemClickListener((parent, view, position, id) -> {
+            posicaoSelecionada = position;
+            editarRefeicao();
         });
 
-        popularListaRefeicao();
+        listViewRefeicao.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                if(actionMode != null){
+                    return false;
+                }
+                posicaoSelecionada = position;
+                viewSelecionada = view;
+                viewSelecionada.setBackgroundColor(Color.RED);
+                listViewRefeicao.setEnabled(false);
+                actionMode = startSupportActionMode(callback);
+                return true;
+            }
+        });
     }
 
-    private void popularListaRefeicao(){
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.lista_refeicao_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int idMenuItem = item.getItemId();
+
+        if (idMenuItem == R.id.menuItemAdicionar) {
+            abrirNovaRefeicao();
+            return true;
+
+        } else if (idMenuItem == R.id.menuItemSobre) {
+            abrirSobre();
+            return true;
+
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void popularListaRefeicao() {
         listaRefeicao = new ArrayList<>();
-
         refeicaoAdapter = new RefeicaoAdapter(this, listaRefeicao);
-
         listViewRefeicao.setAdapter(refeicaoAdapter);
     }
 
-    public void abrirSobre(View view){
-        startActivity(new Intent(this,SobreActivity.class));
-    }
-
-    ActivityResultLauncher<Intent> launcherNovaRefeicao = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-
-                    if(result.getResultCode() == ListaRefeicaoActivity.RESULT_OK){
-                        Intent intent = result.getData();
-
-                        Bundle bundle = intent.getExtras();
-
-                        if(bundle !=null){
-                            int tipoRefeicao = bundle.getInt(RefeicaoActivity.KEY_TIPOREFEICAO);
-                            String data = bundle.getString(RefeicaoActivity.KEY_DATA);
-                            int apetite = bundle.getInt(RefeicaoActivity.KEY_APETITE);
-                            boolean foraHorario = bundle.getBoolean(RefeicaoActivity.KEY_FORAHORARIO);
-
-                            Refeicao refeicao = new Refeicao(tipoRefeicao,data,apetite,foraHorario);
-                            listaRefeicao.add(refeicao);
-                            refeicaoAdapter.notifyDataSetChanged();
-                        }
-
-
-
-                    }
-
-                }
-            }
-    );
-
-    public void abrirNovaRefeicao(View view){
+    private void abrirNovaRefeicao() {
         Intent intentAbertura = new Intent(this, RefeicaoActivity.class);
+        intentAbertura.putExtra(RefeicaoActivity.KEY_MODO, RefeicaoActivity.MODO_NOVO);
         launcherNovaRefeicao.launch(intentAbertura);
     }
 
+    public void abrirSobre() {
+        startActivity(new Intent(this, SobreActivity.class));
+    }
+
+    private void editarRefeicao() {
+        Refeicao refeicao = listaRefeicao.get(posicaoSelecionada);
+
+        Intent intentAbertura = new Intent(this, RefeicaoActivity.class);
+        intentAbertura.putExtra(RefeicaoActivity.KEY_MODO, RefeicaoActivity.MODO_EDITAR);
+        intentAbertura.putExtra(RefeicaoActivity.KEY_TIPOREFEICAO, refeicao.getTipoRefeicao());
+        intentAbertura.putExtra(RefeicaoActivity.KEY_DATA, refeicao.getData());
+        intentAbertura.putExtra(RefeicaoActivity.KEY_APETITE, refeicao.getApetite());
+        intentAbertura.putExtra(RefeicaoActivity.KEY_FORAHORARIO, refeicao.isForaHorario());
+
+        launcherEditarRefeicao.launch(intentAbertura);
+
+    }
+
+    private void excluirRefeicao() {
+        listaRefeicao.remove(posicaoSelecionada);
+        refeicaoAdapter.notifyDataSetChanged();
+    }
 }
--- app/src/main/java/com/lucaspoltronieri/controlealimentar/RefeicaoActivity.java
+++ app/src/main/java/com/lucaspoltronieri/controlealimentar/RefeicaoActivity.java
@@ -2,7 +2,8 @@ package com.lucaspoltronieri.controlealimentar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -10,6 +11,7 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RefeicaoActivity extends AppCompatActivity {
@@ -18,18 +20,72 @@ public class RefeicaoActivity extends AppCompatActivity {
     public static final String KEY_APETITE = "KEY_APETITE";
     public static final String KEY_FORAHORARIO = "KEY_FORAHORARIO";
     public static final String KEY_TIPOREFEICAO = "KEY_TIPOREFEICAO";
+    public static final String KEY_MODO = "MODO";
+    public static final int MODO_NOVO = 0;
+    public static final int MODO_EDITAR = 1;
     private Spinner spinnerTipoRefeicao;
     private EditText editTextDate;
     private RadioGroup radioGroupFome;
     private CheckBox checkBoxForaHorario;
-    private Button buttonSalvar, buttonLimpar;
+    private int modo;
+
+    //limpar campos
+    public void limparCampos(){
+        spinnerTipoRefeicao.setSelection(0);
+        radioGroupFome.clearCheck();
+        checkBoxForaHorario.setChecked(false);
+        editTextDate.setText(null);
+
+        Toast.makeText(this,
+                R.string.formulario_limpo_com_sucesso,
+                Toast.LENGTH_SHORT).show();
+
+        spinnerTipoRefeicao.requestFocus();
+
+    }
+
+    public void salvar(){
+        int tipoRefeicao = spinnerTipoRefeicao.getSelectedItemPosition();
+        String data = editTextDate.getText().toString().trim();
+        //validação
+        if(data.isEmpty()){
+            editTextDate.requestFocus();
+            Toast.makeText(this,
+                    R.string.preencha_a_data_da_refeicao,
+                    Toast.LENGTH_SHORT).show();
+            editTextDate.requestFocus();
+            return;
+        }
+        int selectedRadiId = radioGroupFome.getCheckedRadioButtonId();
+
+        //valida se selecionado algum radioButton
+        if (selectedRadiId == -1){
+            Toast.makeText(this,
+                    R.string.selecione_o_estado_do_apetite,
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //pega o valor selecionado do groupRadio
+        RadioButton rbSelecionado = findViewById(selectedRadiId);
+        int apetite = Integer.parseInt(String.valueOf(rbSelecionado.getTag()));
+
+        boolean foraHorario = checkBoxForaHorario.isChecked();
+
+        Intent intentResposta = new Intent();
+        intentResposta.putExtra(KEY_TIPOREFEICAO, tipoRefeicao);
+        intentResposta.putExtra(KEY_DATA, data);
+        intentResposta.putExtra(KEY_APETITE, apetite);
+        intentResposta.putExtra(KEY_FORAHORARIO, foraHorario);
+
+        setResult(RefeicaoActivity.RESULT_OK, intentResposta);
+        finish();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_refeicao);
-
-        setTitle("Nova Refeição");
 
         //Referência dos elementos que compõe a activity
 
@@ -37,62 +93,62 @@ public class RefeicaoActivity extends AppCompatActivity {
         editTextDate = findViewById(R.id.editTextDate);
         radioGroupFome = findViewById(R.id.radioGroupFome);
         checkBoxForaHorario = findViewById(R.id.checkBoxForaHorario);
-        buttonSalvar = findViewById(R.id.buttonSalvar);
-        buttonLimpar = findViewById(R.id.buttonLimpar);
 
-        //limpar campos
-        buttonLimpar.setOnClickListener(v ->{
-            spinnerTipoRefeicao.setSelection(0);
-            radioGroupFome.clearCheck();
-            checkBoxForaHorario.setChecked(false);
-            editTextDate.setText(null);
+        Intent intentAbertura = getIntent();
+        Bundle bundle = intentAbertura.getExtras();
+        if (bundle !=null){
+            modo = bundle.getInt(KEY_MODO);
 
-            Toast.makeText(this,
-                    R.string.formulario_limpo_com_sucesso,
-                    Toast.LENGTH_SHORT).show();
-
-            spinnerTipoRefeicao.requestFocus();
-
-        });
-
-        //salvar
-        buttonSalvar.setOnClickListener(v ->{
-
-            int tipoRefeicao = spinnerTipoRefeicao.getSelectedItemPosition();
-            String data = editTextDate.getText().toString().trim();
-            //validação
-            if(data.isEmpty()){
-                editTextDate.requestFocus();
-                Toast.makeText(this,
-                        R.string.preencha_a_data_da_refeicao,
-                        Toast.LENGTH_SHORT).show();
-                editTextDate.requestFocus();
-                return;
+            if(modo == MODO_NOVO){
+                setTitle("Nova Refeição");
             }
-            int selectedRadiId = radioGroupFome.getCheckedRadioButtonId();
+            else {
+                setTitle("Editar Refeição");
 
-            //valida se selecionado algum radioButton
-            if (selectedRadiId == -1){
-                Toast.makeText(this,
-                        R.string.selecione_o_estado_do_apetite,
-                        Toast.LENGTH_SHORT).show();
-                return;
+                int tipoRefeicao = bundle.getInt(RefeicaoActivity.KEY_TIPOREFEICAO);
+                String data = bundle.getString(RefeicaoActivity.KEY_DATA);
+                int apetite = bundle.getInt(RefeicaoActivity.KEY_APETITE);
+                boolean foraHorario = bundle.getBoolean(RefeicaoActivity.KEY_FORAHORARIO);
+
+                spinnerTipoRefeicao.setSelection(tipoRefeicao);
+                editTextDate.setText(data);
+                checkBoxForaHorario.setChecked(foraHorario);
+
+                if (apetite == 0) {
+                    ((RadioButton) findViewById(R.id.radioButtonComFome)).setChecked(true);
+                } else {
+                    ((RadioButton) findViewById(R.id.radioButtonSemFome)).setChecked(true);
+                }
+
             }
-            //pega o valor selecionado do groupRadio
-            RadioButton rbSelecionado = findViewById(selectedRadiId);
-            int apetite = Integer.parseInt(String.valueOf(rbSelecionado.getTag()));
+        }
 
-            boolean foraHorario = checkBoxForaHorario.isChecked();
 
-            Intent intentResposta = new Intent();
-            intentResposta.putExtra(KEY_TIPOREFEICAO, tipoRefeicao);
-            intentResposta.putExtra(KEY_DATA, data);
-            intentResposta.putExtra(KEY_APETITE, apetite);
-            intentResposta.putExtra(KEY_FORAHORARIO, foraHorario);
+    }
 
-            setResult(RefeicaoActivity.RESULT_OK, intentResposta);
-            finish();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.refeicao_menu, menu);
+        return true;
+    }
 
-        });
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int idMenuItem = item.getItemId();
+
+        if(idMenuItem == R.id.menuItemAdicionar){
+            salvar();
+            return true;
+
+        } else if (idMenuItem == R.id.menuItemLimpar) {
+            limparCampos();
+            return true;
+
+        }
+        else {
+            return super.onOptionsItemSelected(item);
+        }
+
     }
 }
